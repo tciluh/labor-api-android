29fb34b0daee124ce215aadcd3ab54f93ced8b73
Malte Bargholz <malte@screenri.de>
2017-11-08 23:50:03 +0100

    add interface for delegatable adapters

---
+++ app/src/main/java/de/uni_hannover/htci/labglasses/adapter/ViewTypeDelegateAdapter.kt
@@ -0,0 +1,12 @@
+package de.uni_hannover.htci.labglasses.adapter
+
+import android.support.v7.widget.RecyclerView
+import android.view.ViewGroup
+
+/**
+ * Created by sl33k on 11/8/17.
+ */
+interface ViewTypeDelegateAdapter<T : RecyclerView.ViewHolder> {
+    fun createViewHolder(parent: ViewGroup): T
+    fun bindViewHolder(holder: T, item: ViewType)
+}
