5825bdfeb192b7ac7ea311f039cdb8091b4e097c
Malte Bargholz <malte@screenri.de>
2017-11-08 23:34:45 +0100

    add ViewType interface and constants identifying loading and protocols

---
+++ app/src/main/java/de/uni_hannover/htci/labglasses/adapter/Constants.kt
@@ -0,0 +1,10 @@
+package de.uni_hannover.htci.labglasses.adapter
+
+/**
+ * Created by sl33k on 11/8/17.
+ */
+enum class AdapterType {
+    PROTOCOL, LOADING
+}
+
+
---
+++ app/src/main/java/de/uni_hannover/htci/labglasses/adapter/ViewType.kt
@@ -0,0 +1,9 @@
+package de.uni_hannover.htci.labglasses.adapter
+
+/**
+ * Created by sl33k on 11/8/17.
+ */
+
+interface ViewType {
+    val viewType: AdapterType
+}
