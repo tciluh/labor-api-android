8880b7cdd59e097dc92f56a22e245728125426b6
Malte Bargholz <malte@screenri.de>
2017-11-08 23:51:29 +0100

    implement loading delegatable adapter

---
+++ app/src/main/java/de/uni_hannover/htci/labglasses/adapter/LoadingAdapter.kt
@@ -0,0 +1,24 @@
+package de.uni_hannover.htci.labglasses.adapter
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+import de.uni_hannover.htci.labglasses.R
+import de.uni_hannover.htci.labglasses.utils.inflate
+
+/**
+ * Created by sl33k on 11/8/17.
+ */
+
+class LoadingAdapter : ViewTypeDelegateAdapter<LoadingAdapter.LoadingViewHolder> {
+    override fun createViewHolder(parent: ViewGroup): LoadingViewHolder {
+        val view = parent.inflate(R.layout.protocol_list_loading)
+        return LoadingViewHolder(view)
+    }
+
+    override fun bindViewHolder(holder: LoadingViewHolder, item: ViewType) {
+        //do nothing. everything is laid out in the xml
+    }
+
+    class LoadingViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+}
