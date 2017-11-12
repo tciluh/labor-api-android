ff2f23a4527f77f96d071edc164f97e52a316e5f
Malte Bargholz <malte@screenri.de>
2017-11-12 21:50:58 +0100

    add LaborApi class
    This class provides an preliminary API description for usage with
    RetroFit. It automagically unwraps the API responses and provides a neat
    Observable interface

---
+++ app/src/main/java/de/uni_hannover/htci/labglasses/api/LaborApi.kt
@@ -0,0 +1,20 @@
+package de.uni_hannover.htci.labglasses.api
+
+import com.serjltt.moshi.adapters.Wrapped
+import de.uni_hannover.htci.labglasses.model.Protocol
+import io.reactivex.Single
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+/**
+ * Created by sl33k on 11/12/17.
+ */
+interface LaborApi {
+    @GET("protocol/")
+    @Wrapped(path = arrayOf("payload"))
+    fun getProtocols(): Single<List<Protocol>>
+
+    @GET("protocol/{id}")
+    @Wrapped(path = arrayOf("payload"))
+    fun getProtocol(@Path("id") id: Int): Single<Protocol>
+}
