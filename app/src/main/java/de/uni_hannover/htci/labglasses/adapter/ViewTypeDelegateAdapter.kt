3b6c2d95a96f571b05ebae20602b0aa7d52c1190
Malte Bargholz <malte@screenri.de>
2017-11-12 16:17:47 +0100

    add selection handler to ViewTypeDelegateAdapter

--- app/src/main/java/de/uni_hannover/htci/labglasses/adapter/ViewTypeDelegateAdapter.kt
+++ app/src/main/java/de/uni_hannover/htci/labglasses/adapter/ViewTypeDelegateAdapter.kt
@@ -1,12 +1,17 @@
 package de.uni_hannover.htci.labglasses.adapter
 
 import android.support.v7.widget.RecyclerView
+import android.view.View
 import android.view.ViewGroup
 
 /**
  * Created by sl33k on 11/8/17.
  */
-interface ViewTypeDelegateAdapter {
+interface ViewTypeDelegateAdapter{
+
+    //selection handler consumes an item type and returns nothing
+    val selectHandler: (ViewType) -> Unit
+
     fun createViewHolder(parent: ViewGroup?): RecyclerView.ViewHolder
     fun bindViewHolder(holder: RecyclerView.ViewHolder?, item: ViewType)
 }
