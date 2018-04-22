f09b50d6c542653e8661eaa0ac9478fd1bdf9812
Malte 'sl33k' Bargholz <malte@screenri.de>
2018-04-22 15:01:26 +0200

    use mxparser in euqationFragment and display finished equations

--- app/src/main/java/de/uni_hannover/htci/labglasses/fragments/pager/EquationFragment.kt
+++ app/src/main/java/de/uni_hannover/htci/labglasses/fragments/pager/EquationFragment.kt
@@ -1,5 +1,6 @@
 package de.uni_hannover.htci.labglasses.fragments.pager
 
+import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
@@ -7,18 +8,31 @@ import android.view.View
 import android.view.ViewGroup
 import de.uni_hannover.htci.labglasses.R
 import kotlinx.android.synthetic.main.equation_instruction.*
+import org.mariuszgromada.math.mxparser.Argument
+import org.mariuszgromada.math.mxparser.Constant
 
 /**
  * Created by sl33k on 1/5/18.
+ * This fragment displays a simple math equation with automatic calculation with the passed
+ * measurements map.
  */
 class EquationFragment: Fragment() {
+
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater?.inflate(R.layout.equation_instruction, container, false)
     }
 
     override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        mathView.text = "`${instruction?.equation}`"
+        measurements?.let {
+            val args: List<Argument> = it.map { kv ->
+                Argument(kv.key, kv.value)
+            }
+            val input = instruction?.equation
+            if (input != null) {
+                val expr = Constant(input, *(args.toTypedArray()))
+                mathView.text = "`${expr.constantName} = ${expr.constantValue}`"
+            }
+        }
     }
 }
