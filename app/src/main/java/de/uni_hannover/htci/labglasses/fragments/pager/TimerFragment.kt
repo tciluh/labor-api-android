36212fc9ff1bb400b2271e8b69784ffa2969fac0
Malte Bargholz <malte@screenri.de>
2018-05-18 19:01:45 +0200

    rework timer fragment
    - now supports interface rotation properly
    - on completion the next page will be shown
    - timer pauses/unpauses on page change
    - the reset button is only shown when the timer has run out
    - the timer auto starts when the page becomes visible

--- app/src/main/java/de/uni_hannover/htci/labglasses/fragments/pager/TimerFragment.kt
+++ app/src/main/java/de/uni_hannover/htci/labglasses/fragments/pager/TimerFragment.kt
@@ -1,5 +1,6 @@
 package de.uni_hannover.htci.labglasses.fragments.pager
 
+import android.content.Context
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.support.v4.app.Fragment
@@ -9,74 +10,116 @@ import android.view.View
 import android.view.ViewGroup
 import de.uni_hannover.htci.labglasses.R
 import kotlinx.android.synthetic.main.timer_instruction.*
+import org.jetbrains.anko.support.v4.act
 
 /**
  * Created by sl33k on 1/5/18.
  * this fragment displays a countdown that can be started/stopped and reset by the user.
  */
 
-class TimerFragment: Fragment() {
-    var countdown: CountDownTimer? = null
-    var duration: Long? = null
+class TimerFragment: Fragment(), PagingAwareFragment {
+    interface TimerStepFragmentDelegate {
+        fun onTimerStepFinished()
+    }
 
+    private var countdown: CountDownTimer? = null
+    private var duration: Long = 0
+    private var durationLeft: Long = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // TODO: find out what this is for.
         userVisibleHint = false
     }
+
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val durationString = instruction?.timerDuration
-        if(durationString != null) {
-            try{
-                val converted = durationString.toLong()
-                duration = converted
-                countdown = buildCountdownTimer(converted)
+        if(savedInstanceState == null) {
+            val durationString = instruction?.timerDuration
+            if(durationString != null) {
+                try{
+                    val converted = durationString.toLong() * 1000 // in milliseconds
+                    duration = converted
+                    durationLeft = converted
+                }
+                catch (e: NumberFormatException) {
+                    error("not a valid time in timer duration: ${durationString}")
+                }
             }
-            catch (e: NumberFormatException) {
-                error("not a valid time in timer duration: ${durationString}")
+            else{
+                error("timerDuration is null but a TimerFragment was loaded for this instruction??")
             }
         }
-        else{
-            error("timerDuration is null but a TimerFragment was loaded for this instruction??")
+        else {
+            duration = savedInstanceState.getLong(DURATION_KEY)
+            durationLeft = savedInstanceState.getLong(DURATION_LEFT_KEY)
         }
         return inflater?.inflate(R.layout.timer_instruction, container, false)
     }
-    private fun onTick(millisUntilFinished: Long) {
-        if(timerTextView != null) {
-            val formatted = DateUtils.formatElapsedTime(millisUntilFinished/1000)
-            timerTextView.text = getString(R.string.countdown_timer, formatted)
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        countdown?.cancel()
+        outState?.putLong(DURATION_KEY, duration)
+        outState?.putLong(DURATION_LEFT_KEY, durationLeft)
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        resetButton.visibility = View.GONE
+        resetButton.setOnClickListener{ _ ->
+            resetButton.visibility = View.GONE
+            countdown?.cancel()
+            countdown = buildCountdownTimer(duration)
+            countdown?.start()
+        }
+        if(savedInstanceState != null) {
+            // manually call onPageVisible(), as this will not be done by the pager
+            // this scenario happens if the view is rotated while the timer is running
+            onPageVisible()
         }
     }
+
+    override fun onPageVisible() {
+        if(durationLeft > 1000) {
+            resetButton.visibility = View.GONE
+            countdown = buildCountdownTimer(durationLeft)
+            countdown?.start()
+        }
+    }
+
+    override fun onPageHidden() {
+        countdown?.cancel()
+    }
+
+    private fun onTick(millisUntilFinished: Long) {
+        durationLeft = millisUntilFinished
+        updateVisibleProgress()
+    }
+
     private fun onFinish() {
-        //TODO: not sure what to here yet
+        resetButton.visibility = View.VISIBLE
+        (this.activity as? TimerStepFragmentDelegate)?.onTimerStepFinished()
+    }
+
+
+    private fun updateVisibleProgress() {
+        timerTextView?.let {
+            val formatted = DateUtils.formatElapsedTime(durationLeft/1000)
+            it.text = getString(R.string.countdown_timer, formatted)
+        }
     }
 
     private fun buildCountdownTimer(duration: Long) =
-            object: CountDownTimer(duration*1000, 500) {
+            object: CountDownTimer(duration, 500) {
                     override fun onTick(millisUntilFinished: Long) = this@TimerFragment.onTick(millisUntilFinished)
                     override fun onFinish() = this@TimerFragment.onFinish()
                 }
 
-    private fun initTextView() = timerTextView?.let {
-        it.text = getString(R.string.countdown_timer, DateUtils.formatElapsedTime(duration ?: 0))
+
+    companion object {
+        const val DURATION_KEY = "timer-fragment-saved-duration"
+        const val DURATION_LEFT_KEY = "timer-fragment-duration-left"
     }
 
-
-    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        initTextView()
-        startButton.setOnClickListener { _ ->
-            countdown?.start()
-        }
-        resetButton.setOnClickListener{ _ ->
-            countdown?.cancel()
-            duration?.let {
-                countdown = buildCountdownTimer(it)
-                initTextView()
-                countdown?.start()
-            }
-        }
-
-    }
 
 }
